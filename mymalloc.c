af8587863e82d480111851c24f3aad5153ec6d03
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-06-23 22:15:30 -0400

    Initial commit

---
+++ mymalloc.c
@@ -0,0 +1,14 @@
+// Vous devez modifier ce fichier pour le TP 2
+// Tous votre code doit être dans ce fichier
+
+#include "mymalloc.h"
+
+void *mymalloc(size_t size){
+  // À modifier
+  return NULL;
+}
+
+void myfree(void *ptr){
+  // À modifier
+  return;
+}
