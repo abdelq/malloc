9522bd8c6336370d65003e6f23b9e71aa609814d
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-07-02 00:26:19 -0400

    Lower time of test 6

--- tests.c
+++ tests.c
@@ -76,7 +76,8 @@ int test6(){
 
   // Création d'une liste doublement chaînée
   node *current = root;
-  for(int i = 0; i < 4 * 1024 * 1024; i ++){
+  for(int i = 0; i < 4 * 1024; i ++){
+  /*for(int i = 0; i < 4 * 1024 * 1024; i ++){*/
     node *p = mymalloc(sizeof(node));
     p->valueInt = i;
     p->valueChar = 115;
