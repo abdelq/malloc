52e6141b69f187ea18420ca8f766055eed8c7ef5
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-07-02 00:24:55 -0400

    Indenting

--- mymalloc.c
+++ mymalloc.c
@@ -9,90 +9,94 @@
         PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)
 
 typedef struct block {
-    size_t         size;
-    unsigned short free : 1;
-    struct block   *next;
-    void           *data;
+	size_t size;
+	unsigned short free:1;
+	struct block *next;
+	void *data;
 } block;
 
 void *first_block = NULL;
 
-block *extend(size_t size) {
-    block *b = MMAP(size);
+block *extend(size_t size)
+{
+	block *b = MMAP(size);
 
-    if (b == MAP_FAILED) {
-        return NULL;
-    }
+	if (b == MAP_FAILED) {
+		return NULL;
+	}
 
-    b -> size = size;
-    b -> free = FALSE;
-    b -> next = NULL;
-    b -> data = b + sizeof(block);
+	b->size = size;
+	b->free = FALSE;
+	b->next = NULL;
+	b->data = b + sizeof(block);
 
-    return b;
+	return b;
 }
 
-void *mymalloc(size_t size) {
-    if (!size) {
-        return NULL;
-    }
+void *mymalloc(size_t size)
+{
+	if (!size) {
+		return NULL;
+	}
 
-    block *b = NULL;
+	block *b = NULL;
 
-    if (first_block) {
+	if (first_block) {
 #if DEBUG
-        printf("First block found\n");
+		printf("First block found\n");
 #endif
 
-        block *free_block = first_block;
-        block *last_block = first_block;
+		block *free_block = first_block;
+		block *last_block = first_block;
 
-        // Find a free block
-        while (free_block && !(free_block -> free && free_block -> size >= size)) {
-            last_block = free_block;
-            free_block = free_block -> next;
-        }
+		// Find a free block
+		while (free_block
+		       && !(free_block->free && free_block->size >= size)) {
+			last_block = free_block;
+			free_block = free_block->next;
+		}
 
-        if (free_block) {
-            b = free_block;
-            b -> free = FALSE;
-        } else {
-            b = extend(size);
+		if (free_block) {
+			b = free_block;
+			b->free = FALSE;
+		} else {
+			b = extend(size);
 
-            if (b == NULL) {
-                return NULL;
-            }
+			if (b == NULL) {
+				return NULL;
+			}
 
-            last_block -> next = b;
-        }
-    } else {
+			last_block->next = b;
+		}
+	} else {
 #if DEBUG
-        printf("No first block found\n");
+		printf("No first block found\n");
 #endif
 
-        b = extend(size);
+		b = extend(size);
 
-        if (b == NULL) {
-            return NULL;
-        }
+		if (b == NULL) {
+			return NULL;
+		}
 
-        first_block = b;
-    }
+		first_block = b;
+	}
 
 #if DEBUG
-        printf("Address of b: %p\n", b);
-        printf("Address of b -> data: %p\n", b -> data);
+	printf("Address of b: %p\n", b);
+	printf("Address of b -> data: %p\n", b->data);
 #endif
 
-    return b -> data;
+	return b->data;
 }
 
-void myfree(void *ptr) {
-    if (ptr == NULL) {
-        return;
-    }
+void myfree(void *ptr)
+{
+	if (ptr == NULL) {
+		return;
+	}
 
-    block *b = ptr - sizeof(block) * sizeof(block);
+	block *b = ptr - sizeof(block) * sizeof(block);
 
-    b -> free = TRUE;
+	b->free = TRUE;
 }
