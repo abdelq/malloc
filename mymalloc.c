463a9cb03ba8b0d3b71604c4ac3b23f1396b6129
David-Bambi <davidboivinbambi@gmail.com>
2017-07-09 01:23:59 -0400

    unlink

--- mymalloc.c
+++ mymalloc.c
@@ -12,10 +12,13 @@ typedef struct block {
 	size_t size;
 	unsigned short free:1;
 	struct block *next;
+        struct block *nextFree;
 	void *data;
 } block;
 
 void *first_block = NULL;
+block *freeBlocks = NULL;
+block *last_block = NULL;
 
 block *extend(size_t size)
 {
@@ -28,6 +31,7 @@ block *extend(size_t size)
 	b->size = size;
 	b->free = FALSE;
 	b->next = NULL;
+        b->nextFree =NULL;
 	b->data = b + sizeof(block);
 
 	return b;
@@ -42,19 +46,21 @@ void *mymalloc(size_t size)
 	block *b = NULL;
 
 	if (first_block) {
-		block *free_block = first_block;
-		block *last_block = first_block;
-
+		block *free_block = freeBlocks;
+		block *last_free_block = freeBlocks;
+                
 		// Find a free block
-		while (free_block
-		       && !(free_block->free && free_block->size >= size)) {
-			last_block = free_block;
-			free_block = free_block->next;
+		while ( free_block && size >= free_block->size) {
+			last_free_block = free_block;
+			free_block = free_block->nextFree;
 		}
 
-		if (free_block) {
+		if (free_block)
+                  {
+                        last_free_block->nextFree = free_block->nextFree;
 			b = free_block;
 			b->free = FALSE;
+                        
 		} else {
 			b = extend(size);
 
@@ -62,8 +68,10 @@ void *mymalloc(size_t size)
 				return NULL;
 			}
 
-			last_block->next = b;
-		}
+                      
+		        last_block->next = b;
+                        last_block = b;
+		}                                    
 	} else {
 		b = extend(size);
 
@@ -71,7 +79,10 @@ void *mymalloc(size_t size)
 			return NULL;
 		}
 
-		first_block = b;
+                first_block = b;
+                last_block = b;
+                
+                 
 	}
 
 #if DEBUG
@@ -88,7 +99,39 @@ void myfree(void *ptr)
 		return;
 	}
 
-	block *b = (block *) ptr - sizeof(block);
+	block *b = (block *) ptr;
 
 	b->free = TRUE;
+
+
+ 
+        if (freeBlocks == NULL)
+            freeBlocks = b;
+        else
+          {
+            block *blck = first_block;
+            block *freeBlck = freeBlocks;
+            block *lastFree = NULL;
+            while(blck != b && freeBlck != NULL)
+              {
+                if (blck == freeBlck)
+                  {
+                    lastFree = freeBlck;
+                    freeBlck = freeBlck->next;
+                  }
+                blck = blck->next;
+              }
+
+            if(freeBlck != NULL)
+              {
+                b->nextFree = freeBlck;
+                lastFree->nextFree = b;
+              }
+            else
+                lastFree->nextFree = b;
+              
+            
+          }
+          
+        
 }
